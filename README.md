11ecdd722ae578b0c134c5345c2101340343ac7c
Sean-Jiun Wang <churchguitarist@gmail.com>
2018-08-08 15:21:19 -0500

    Setup UF dissertation template on the master branch

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# phd-thesis
-Repository for my PhD thesis (work in progress)
+This is a throwaway repo to test out Travis-CI.
+
+[![Build Status](https://travis-ci.org/swang373/expert-parakeet.svg?branch=master)](https://travis-ci.org/swang373/expert-parakeet)
---
+++ before_deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Make sure the current working directory is the build directory.
+cd "$TRAVIS_BUILD_DIR"
+
+# Fetch all other remote branches.
+git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
+
+# Stash the build products.
+git stash --include-untracked
+
+# Checkout the gh-pages branch.
+git checkout -b gh-pages origin/gh-pages
+
+# Remove the previous dissertation version, replace it with the
+# latest version built by Travis-CI, and clean up extra files.
+rm main.pdf
+git stash pop
+git clean -fxd
+
